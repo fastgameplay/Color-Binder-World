43c1537a0dd09668795fede56108376e29503102
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2024-07-18 15:58:13 +0400

    Moved Camera Adjustment to Editor Script

--- Assets/Scripts/Camera/CameraHolder.cs
+++ Assets/Scripts/Camera/CameraHolder.cs
@@ -7,8 +7,10 @@ namespace Camera
     public class CameraHolder : DontDestroyMonoBehaviourSingleton<CameraHolder>
     {
         public UnityEngine.Camera MainCamera => mainCamera;
-        [HideInInspector] public Vector2 BoundaryMin = new Vector2(-10, -10);
-        [HideInInspector] public Vector2 BoundaryMax = new Vector2(10, 10);
+        [HideInInspector] public Vector2 ScreenBoundaryMin = new Vector2(-10, -10);
+        [HideInInspector] public Vector2 ScreenBoundaryMax = new Vector2(10, 10);
+        [HideInInspector] public Vector2 AdjustedBoundaryMin = new Vector2(10, 0);
+        [HideInInspector] public Vector2 AdjustedBoundaryMax;
         [field: SerializeField] public bool ShowGizmos {get; private set;} = true;
 
         [SerializeField] private UnityEngine.Camera mainCamera;
@@ -17,18 +19,13 @@ namespace Camera
 
         private Vector3 startDragPosition;
         private Vector3 endDragPosition;
-        [HideInInspector] private Vector2 adjustedBoundaryMin;
-        [HideInInspector] private Vector2 adjustedBoundaryMax;
 
         private void Start()
         {
             // Set up the drag event handlers from ClickHandler
             ClickHandler.Instance.SetDragEventHandlers(OnDragStart, OnDragEnd);
         }
-        private void OnValidate()
-        {
-            UpdateAdjustedBoundaries();
-        }
+
         private void OnDragStart(Vector3 startPosition)
         {
             startDragPosition = startPosition;
@@ -50,22 +47,15 @@ namespace Camera
                 MoveCameraInDirection(direction);
             }
         }
-        private void UpdateAdjustedBoundaries()
-        {
-            float cameraHeight = mainCamera.orthographicSize * 2f;
-            float cameraWidth = cameraHeight * mainCamera.aspect;
 
-            adjustedBoundaryMin = new Vector2(BoundaryMin.x + cameraWidth / 2, BoundaryMin.y + cameraHeight / 2);
-            adjustedBoundaryMax = new Vector2(BoundaryMax.x - cameraWidth / 2, BoundaryMax.y - cameraHeight / 2);
-        }
         private void MoveCameraInDirection(Vector3 direction)
         {
             Vector3 movement = new Vector3(direction.x, direction.y, 0) * cameraMoveSpeed;
             Vector3 newPosition = mainCamera.transform.position + movement;
 
             // Clamp the camera's position to stay within the boundary
-            newPosition.x = Mathf.Clamp(newPosition.x, adjustedBoundaryMin.x, adjustedBoundaryMax.x);
-            newPosition.y = Mathf.Clamp(newPosition.y, adjustedBoundaryMin.y, adjustedBoundaryMax.y);
+            newPosition.x = Mathf.Clamp(newPosition.x, AdjustedBoundaryMin.x, AdjustedBoundaryMax.x);
+            newPosition.y = Mathf.Clamp(newPosition.y, AdjustedBoundaryMin.y, AdjustedBoundaryMax.y);
 
             mainCamera.transform.position = newPosition;
         }
--- Assets/Scripts/Camera/Editor/CameraHolderEditor.cs
+++ Assets/Scripts/Camera/Editor/CameraHolderEditor.cs
@@ -1,36 +1,50 @@
 using UnityEditor;
 using UnityEngine;
-
-[CustomEditor(typeof(Camera.CameraHolder))]
-public class CameraHolderEditor : Editor
+namespace Camera
 {
-    private void OnSceneGUI()
+    [CustomEditor(typeof(CameraHolder))]
+    public class CameraHolderEditor : Editor
     {
-        Camera.CameraHolder cameraHolder = (Camera.CameraHolder)target;
-
-        if (!cameraHolder.ShowGizmos) return;
-
-        Vector3 bottomLeft = new Vector3(cameraHolder.BoundaryMin.x, cameraHolder.BoundaryMin.y, cameraHolder.MainCamera.transform.position.z);
-        Vector3 topRight = new Vector3(cameraHolder.BoundaryMax.x, cameraHolder.BoundaryMax.y, cameraHolder.MainCamera.transform.position.z);
-
-        Handles.color = Color.red;
-        Handles.DrawLine(bottomLeft, new Vector3(bottomLeft.x, topRight.y, cameraHolder.MainCamera.transform.position.z));
-        Handles.DrawLine(new Vector3(bottomLeft.x, topRight.y, cameraHolder.MainCamera.transform.position.z), topRight);
-        Handles.DrawLine(topRight, new Vector3(topRight.x, bottomLeft.y, cameraHolder.MainCamera.transform.position.z));
-        Handles.DrawLine(new Vector3(topRight.x, bottomLeft.y, cameraHolder.MainCamera.transform.position.z), bottomLeft);
-
-        EditorGUI.BeginChangeCheck();
-        Vector3 newBottomLeft = Handles.PositionHandle(bottomLeft, Quaternion.identity);
-        Vector3 newTopRight = Handles.PositionHandle(topRight, Quaternion.identity);
-        if (EditorGUI.EndChangeCheck())
+        private void OnSceneGUI()
         {
-            Undo.RecordObject(cameraHolder, "Change Camera Boundary");
+            CameraHolder cameraHolder = (CameraHolder)target;
 
-            cameraHolder.BoundaryMin = new Vector2(newBottomLeft.x, newBottomLeft.y);
-            cameraHolder.BoundaryMax = new Vector2(newTopRight.x, newTopRight.y);
+            if (!cameraHolder.ShowGizmos) return;
 
-            cameraHolder.BoundaryMin = new Vector2(Mathf.Min(cameraHolder.BoundaryMin.x, cameraHolder.BoundaryMax.x), Mathf.Min(cameraHolder.BoundaryMin.y, cameraHolder.BoundaryMax.y));
-            cameraHolder.BoundaryMax = new Vector2(Mathf.Max(cameraHolder.BoundaryMin.x, cameraHolder.BoundaryMax.x), Mathf.Max(cameraHolder.BoundaryMin.y, cameraHolder.BoundaryMax.y));
+            Vector3 bottomLeft = new Vector3(cameraHolder.ScreenBoundaryMin.x, cameraHolder.ScreenBoundaryMin.y, cameraHolder.MainCamera.transform.position.z);
+            Vector3 topRight = new Vector3(cameraHolder.ScreenBoundaryMax.x, cameraHolder.ScreenBoundaryMax.y, cameraHolder.MainCamera.transform.position.z);
+
+            Handles.color = Color.red;
+            Handles.DrawLine(bottomLeft, new Vector3(bottomLeft.x, topRight.y, cameraHolder.MainCamera.transform.position.z));
+            Handles.DrawLine(new Vector3(bottomLeft.x, topRight.y, cameraHolder.MainCamera.transform.position.z), topRight);
+            Handles.DrawLine(topRight, new Vector3(topRight.x, bottomLeft.y, cameraHolder.MainCamera.transform.position.z));
+            Handles.DrawLine(new Vector3(topRight.x, bottomLeft.y, cameraHolder.MainCamera.transform.position.z), bottomLeft);
+
+            EditorGUI.BeginChangeCheck();
+            Vector3 newBottomLeft = Handles.PositionHandle(bottomLeft, Quaternion.identity);
+            Vector3 newTopRight = Handles.PositionHandle(topRight, Quaternion.identity);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(cameraHolder, "Change Camera Boundary");
+
+                cameraHolder.ScreenBoundaryMin = new Vector2(newBottomLeft.x, newBottomLeft.y);
+                cameraHolder.ScreenBoundaryMax = new Vector2(newTopRight.x, newTopRight.y);
+
+                cameraHolder.ScreenBoundaryMin = new Vector2(Mathf.Min(cameraHolder.ScreenBoundaryMin.x, cameraHolder.ScreenBoundaryMax.x), Mathf.Min(cameraHolder.ScreenBoundaryMin.y, cameraHolder.ScreenBoundaryMax.y));
+                cameraHolder.ScreenBoundaryMax = new Vector2(Mathf.Max(cameraHolder.ScreenBoundaryMin.x, cameraHolder.ScreenBoundaryMax.x), Mathf.Max(cameraHolder.ScreenBoundaryMin.y, cameraHolder.ScreenBoundaryMax.y));
+
+                UpdateAdjustedBoundaries(cameraHolder);
+            }
+        }
+
+        private void UpdateAdjustedBoundaries(CameraHolder cameraHolder)
+        {
+            float cameraHeight = cameraHolder.MainCamera.orthographicSize * 2f;
+            float cameraWidth = cameraHeight * cameraHolder.MainCamera.aspect;
+
+            cameraHolder.AdjustedBoundaryMin = new Vector2(cameraHolder.ScreenBoundaryMin.x + cameraWidth / 2, cameraHolder.ScreenBoundaryMin.y + cameraHeight / 2);
+            cameraHolder.AdjustedBoundaryMax = new Vector2(cameraHolder.ScreenBoundaryMax.x - cameraWidth / 2, cameraHolder.ScreenBoundaryMax.y - cameraHeight / 2);
+
         }
     }
 }
